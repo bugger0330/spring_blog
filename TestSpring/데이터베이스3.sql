e2d3b0e802a00bd3f377cbf83b6843a2e0179255
bugger0330 <bugger0330@naver.com>
2022-06-24 09:15:18 +0900

    db

--- TestSpring/데이터베이스3.sql
+++ TestSpring/데이터베이스3.sql
@@ -14,12 +14,10 @@
 
 
 -- test 데이터베이스 구조 내보내기
-DROP DATABASE IF EXISTS `test`;
 CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
 USE `test`;
 
 -- 테이블 test.board_mst 구조 내보내기
-DROP TABLE IF EXISTS `board_mst`;
 CREATE TABLE IF NOT EXISTS `board_mst` (
   `board_code` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -27,12 +25,74 @@ CREATE TABLE IF NOT EXISTS `board_mst` (
   `content` text DEFAULT NULL,
   `count` int(11) DEFAULT NULL,
   PRIMARY KEY (`board_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=157 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=194 DEFAULT CHARSET=utf8mb3;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 test.order1 구조 내보내기
+CREATE TABLE IF NOT EXISTS `order1` (
+  `product_code` int(11) NOT NULL,
+  `product_img1` varchar(500) DEFAULT NULL,
+  `product_title` varchar(500) NOT NULL,
+  `product_price` varchar(500) NOT NULL,
+  `username` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 test.order2 구조 내보내기
+CREATE TABLE IF NOT EXISTS `order2` (
+  `product_code` int(11) DEFAULT NULL,
+  `product_img1` varchar(500) DEFAULT NULL,
+  `product_title` varchar(500) DEFAULT NULL,
+  `product_price` varchar(500) DEFAULT NULL,
+  `username` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 test.order3 구조 내보내기
+CREATE TABLE IF NOT EXISTS `order3` (
+  `order_code` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `phone2` varchar(50) NOT NULL,
+  `address_num` varchar(50) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `address2` varchar(500) NOT NULL,
+  `requests` varchar(500) NOT NULL,
+  `all_price` int(11) NOT NULL,
+  PRIMARY KEY (`order_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 test.product 구조 내보내기
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_code` int(11) NOT NULL AUTO_INCREMENT,
+  `product_username` varchar(50) NOT NULL,
+  `product_phone` varchar(50) DEFAULT NULL,
+  `product_title` varchar(50) NOT NULL,
+  `product_content` varchar(500) NOT NULL,
+  `product_price` varchar(500) NOT NULL,
+  `product_delivery` varchar(500) DEFAULT NULL COMMENT '결제방법 - 택배',
+  `product_status` varchar(500) DEFAULT NULL COMMENT '제품상태',
+  `product_exchange` varchar(500) DEFAULT NULL COMMENT '교환/환불',
+  `product_select` varchar(500) DEFAULT NULL COMMENT '카테고리',
+  `product_img1` varchar(500) DEFAULT NULL,
+  `product_img2` varchar(500) DEFAULT NULL,
+  `product_img3` varchar(500) DEFAULT NULL,
+  `product_img4` varchar(500) DEFAULT NULL,
+  `product_img5` varchar(500) DEFAULT NULL,
+  `product_img6` varchar(500) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`product_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb3;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 test.user_dtl 구조 내보내기
-DROP TABLE IF EXISTS `user_dtl`;
 CREATE TABLE IF NOT EXISTS `user_dtl` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `usercode` int(11) NOT NULL,
@@ -44,26 +104,28 @@ CREATE TABLE IF NOT EXISTS `user_dtl` (
   UNIQUE KEY `username` (`username`),
   KEY `usercode_for_user_detail` (`usercode`),
   CONSTRAINT `usercode_for_user_detail` FOREIGN KEY (`usercode`) REFERENCES `user_mst` (`usercode`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb3;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 test.user_mst 구조 내보내기
-DROP TABLE IF EXISTS `user_mst`;
 CREATE TABLE IF NOT EXISTS `user_mst` (
   `usercode` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL DEFAULT '0',
-  `password` varchar(100) NOT NULL DEFAULT '0',
-  `name` varchar(100) NOT NULL DEFAULT '0',
-  `phone` varchar(100) NOT NULL DEFAULT '0',
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `nickname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `address2` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`usercode`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1020 DEFAULT CHARSET=utf8mb3;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 트리거 test.user_mst_after_insert 구조 내보내기
-DROP TRIGGER IF EXISTS `user_mst_after_insert`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
 CREATE TRIGGER `user_mst_after_insert` AFTER INSERT ON `user_mst` FOR EACH ROW BEGIN
@@ -73,9 +135,9 @@ CREATE TRIGGER `user_mst_after_insert` AFTER INSERT ON `user_mst` FOR EACH ROW B
 		0,
 		NEW.usercode,
 		NEW.username,
-		'닉네임',
-		'주소',
-		'1'
+		NEW.nickname,
+		NEW.address,
+		NULL
 	);
 	
 END//
@@ -83,7 +145,6 @@ DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 -- 트리거 test.user_mst_before_delete 구조 내보내기
-DROP TRIGGER IF EXISTS `user_mst_before_delete`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
 CREATE TRIGGER `user_mst_before_delete` BEFORE DELETE ON `user_mst` FOR EACH ROW BEGIN
